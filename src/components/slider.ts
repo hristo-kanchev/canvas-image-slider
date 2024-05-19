18f32893912727e9a58f9024b981d8e8824a6122
Hristo Kanchev <hristokkanchev@gmail.com>
2024-05-19 14:16:47 +0300

    fix(image-slider): Fixed issue with loading images upon serving prod dist

--- src/components/slider.ts
+++ src/components/slider.ts
@@ -3,25 +3,30 @@ import { Vector2, calculateScaleFactor } from "../helpers";
 import { InputEventType, attachToInputEvent, getInputState } from "../input";
 import { clearRenderTarget, drawFrame, drawImage, getRenderer } from "../renderer";
 
+import slide0Url from "../../assets/0.jpg";
+import slide1Url from "../../assets/1.jpg";
+import slide2Url from "../../assets/2.jpg";
+import slide3Url from "../../assets/3.jpg";
+
 export async function initImageSlider() {
     const renderer = getRenderer();
 
     loadAssets([
         await addAsset({
             type: AssetType.Image,
-            path: "./assets/0.jpg",
+            path: slide0Url,
         }),
         await addAsset({
             type: AssetType.Image,
-            path: "./assets/1.jpg",
+            path: slide1Url,
         }),
         await addAsset({
             type: AssetType.Image,
-            path: "./assets/2.jpg",
+            path: slide2Url,
         }),
         await addAsset({
             type: AssetType.Image,
-            path: "./assets/3.jpg",
+            path: slide3Url,
         }),
     ]);
 
